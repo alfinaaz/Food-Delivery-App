3860514fc24389e88b099e78ea775cf79fcf3f08
alfinaaz <alfi2410work@gmail.com>
2022-03-12 20:48:32 +0530

    Implementing Stripe Payment in frontend, getting the token in the console.

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,9 +1,20 @@
 import React from "react";
 import StripeCheckout from 'react-stripe-checkout';
-export default function Checkout(){
-return (
+export default function Checkout({subtotal}){
+function tokenHandler(token)
+{
+    console.log(token);
+}
+
+    return (
   <div>
-      <StripeCheckout>
+      <StripeCheckout amount={subtotal*100}
+      shippingAddress
+      token= {tokenHandler}
+      currency='INR'
+      stripeKey="pk_test_51KcWNhSDpwaic1qABSURg9F8dYR9NIL0aZP9FG8DdupAy2qn5a7JGyGidd79mUUSXS5vJg1rFDYXFlD0obKjA76b00kwO8ekLz"
+      >
+
 
       <button className="btn">Pay Now</button>
 
