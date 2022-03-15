b4cc18f97646845bddb12c9a7ab0c46683406fe4
alfinaaz <alfi2410work@gmail.com>
2022-03-15 21:30:56 +0530

    "Integrating Razor pay with backend and front end , verifying the payment process throuh SHA256 algorithm

--- client/src/Screen/CartScreen.js
+++ client/src/Screen/CartScreen.js
@@ -7,6 +7,7 @@ import Checkout1 from '../components/Checkout1'
 
 
 
+
 export default  function CartScreen() {
    
    
--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -1,67 +1,84 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
-const Razorpay=require('razorpay');
-const store =require('store2');
+const Razorpay = require('razorpay');
+const crypto = require('crypto');
+const store = require('store2');
 const { v4: uuidv4 } = require('uuid');
-const razorpay = new Razorpay({ key_id:'rzp_test_5teoHf9kBKHnQ2', key_secret: 'Ujdr3fQqLYdjf5L3rKRRD4Gl' })
+const razorpay = new Razorpay({ key_id: 'rzp_test_5teoHf9kBKHnQ2', key_secret: 'Ujdr3fQqLYdjf5L3rKRRD4Gl' })
 
 
+router.post('/payment', async (req, res,) => {
 
-//router.post('/placeorder', async(req, res,) => {
-
-  /* const customer = {
-	name: req.body.customer.name,
-	email: req.body.customer.email,
-	phone: req.body.customer.phone,
-	address:req.body.customer.address,
-	city:req.body.customer.city,
-	state:req.body.customer.state,
-	zip:req.body.customer.zip
-     }*/
-    // subtotal= req.body.subtotal;
-	//const cartItems=req.body.cartItems;
-	//const currentUser=req.body.currentUser
 	
-	 //store('subtotal',subtotal);
-       
-//	}
+	const amount = req.body.subtotal*100;
+	const currency = 'INR'
+
+	const options = {
+		amount,
+		currency
+	}
+
+	try {
+		const response = await razorpay.orders.create(options)
+		console.log(response)
+		res.json({
+			id: response.id,
+			currency: response.currency,
+			amount: response.amount
+		})
+	} catch (error) {
+		console.log(error)
+	}
+})
+router.post('/success', async (req, res) => {
+
+
+
+	try {
 	
-//) 
-router.post('/payment', async(req, res,) => {
-
-
-	console.log(req.body.subtotal);
-	//const subtotal = store('subtotal') ? store('subtotal'):0
-	  const amount = req.body.subtotal*100;
-	  const currency = 'INR'
+		// getting the details back from our font-end
+		const {
+			orderId,
+			razorpayPaymentId,
+			razorpayOrderId,
+			razorpaySignature
+		  }=req.body.orderdata;
   
-	  const options = {
-		  amount,
-		  currency
-		  //receipt:uuidv4(),
-		  /*notes: {
-			  name: customer.name,
-			  phone: customer.phone,
-			  email:customer.email,
-			  address:customer.address,
-			  city:customer.city,
-			  state:customer.state,
-			  zip:customer.zip,
-			  
-		   }*/ 
-	  }
-  
-	  try {
-		  const response = await razorpay.orders.create(options)
-		  console.log(response)
-		  res.json({
-			  id: response.id,
-			  currency: response.currency,
-			  amount: response.amount
-		  })
-	  } catch (error) {
-		  console.log(error)
-	  } 
-  }) 
+		  const{ name,email,phone,address,city,state,zip}=req.body.customer;
 
-module.exports=router;
+		// Creating our own digest
+		// The format should be like this:
+		//  digest = hmac_sha256(orderCreationId + "|" + razorpayPaymentId,'secret id');
+
+
+		const hash = crypto.createHmac('sha256', "Ujdr3fQqLYdjf5L3rKRRD4Gl").update(`${orderId}|${razorpayPaymentId}`).digest('hex');
+
+		// comaparing our digest with the actual signature
+	
+		if (hash === razorpaySignature){
+			   res.json("SUCESS");
+			   console.log("SUCCESS");
+			   console.log(req.body.orderdata);
+				console.log(req.body.customer);
+
+		}
+	
+			   // THE PAYMENT IS LEGIT & VERIFIED
+			// YOU CAN SAVE THE DETAILS IN YOUR DATABASE IF YOU WANT
+
+		else{
+				res.json("FAILURE");
+				consolr.log("FAILURE");
+				console.log(req.body.orderdata);
+				console.log(req.body.customer);
+		}
+}
+	
+	catch (error) {
+		res.status(500).send(error);
+	}
+  
+});
+
+module.exports = router;
